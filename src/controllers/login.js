90a744efb8fb8894ecd45eba213f96204893ab51
Sergio Zegarra <s.zegarra@hotmail.com>
2022-05-16 18:50:22 -0500

    se agrego la parte de validacion del token para el acceso

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -14,8 +14,17 @@ export const checkLogin = async (req, res) => {
     console.log(md5(req.body.pass));
     const [rows] = await db.query("SELECT * FROM tbl_usuarios WHERE correo=? AND pass=?",[req.body.uzer,req.body.pass])
     //res.send("esto es para insertar\n id: "+req.body.id);
-    if(rows.length==1)
+    if(rows.length==1){
+        try {
+        const token = md5(req.body.dev)+" "+md5(req.body.dev+rows.admin);
+        const rt = await db.query("INSERT INTO tbl_token values(0,?,?,?,UNIX_TIMESTAMP(NOW()))" ,[rows[0].admin,req.body.dev,token],);
+        console.log(rt);
         res.json(rows);
+
+        }catch(err){                      
+            res.status(500).send({success:false, message:err});
+        }    
+    }        
     else
         res.sendStatus(404);
 }
